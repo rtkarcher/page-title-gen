9c6c591134e6cd7245ea0aa900e094256bd08ae3
Rainer Karcher <Rainer.Karcher@Cerner.com>
2014-08-27 11:57:03 -0500

    Initial commit

---
+++ page_title_gen.php
@@ -0,0 +1,7 @@
+<?php /* Page Title Generator */
+$currentFile = $_SERVER["PHP_SELF"];
+$parts = explode('/', $currentFile);
+$parts = $parts[count($parts) - 1];
+$name = explode('.', $parts);
+unset ($name[count($name) - 1]);
+$pageName = implode('.',$name);
