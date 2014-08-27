51df1bfc3bd6853d7232f80017573098f01600a4
Rainer Karcher <Rainer.Karcher@Cerner.com>
2014-08-27 11:59:41 -0500

    Descriptive comments

--- page_title_gen.php
+++ page_title_gen.php
@@ -1,5 +1,12 @@
 <?php /* Page Title Generator */
+
 $currentFile = $_SERVER["PHP_SELF"];
+
+/* The 'explode' and 'implode' string functions allow you to remove 
+/* the file path information for the current file, shave off the .php 
+/* file extension, and then echo the file name sans extension, with 
+/* 'ucwords' acting to capitalize the first character of the string. */
+
 $parts = explode('/', $currentFile);
 $parts = $parts[count($parts) - 1];
 $name = explode('.', $parts);
